030fa7daa202aaf72b2511d565944d9e2cf8a487
Marwan <marwan@thisisparachute.com>
2018-04-23 17:04:49 +0100

    'dhfd'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1><br>`,
+  template: `<h1>Hello {{name}}</h1><br>
+  Version: <span id="version">v0.1.9</span>
+      <!-- the button onClick sends a quitAndInstall message to the electron main process -->
+  <button id="ready" onClick="ipcRenderer.send('quitAndInstall')">no updates ready</button>
+  `,
 })
 export class AppComponent  {
-   name = 'Parchute';
+   name = 'Marwan';
  }
